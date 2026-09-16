f349146bb5e748ec1d5475d1be05b3288051d70d
hyering126 <127617801+hyering126@users.noreply.github.com>
2026-09-16 13:52:24 +0900

    게시글 목록 내림차순(최신순)으로 적용

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +25,7 @@ public class BoardService {
 
     public Page<Board> findBoardListWithCri(Criteria cri) {
         int page = cri.getPageNum() < 1 ? 0 : cri.getPageNum() - 1;
-        return boardRepository.findAll(PageRequest.of(page, cri.getAmount()));
+        return boardRepository.findAll(PageRequest.of(page, cri.getAmount(), Sort.by("createdDate").descending()));
     }
 
     public int getTotalCount() {
